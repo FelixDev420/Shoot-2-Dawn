40fb152ec4bcd2f45276c158663f46e908d33524
GamingTrend <rafeole123@gmail.com>
2017-10-29 17:14:10 +0100

    Create EnemyPermission.py

---
+++ Permission/EnemyPermission.py
@@ -0,0 +1,19 @@
+if a == Player
+   attack = True
+else:
+   attack = False
+if a == Player
+   print('Shoot him!')
+or
+   print('Damn kill him!')
+or
+   print('What is he doing here!?')
+while True:
+    stay()
+if a == Player
+   stay() = False
+if a == Player
+   follow = True
+else:
+   follow = False
+Enemy_fast = 5
