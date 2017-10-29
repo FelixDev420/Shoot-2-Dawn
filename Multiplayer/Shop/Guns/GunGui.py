9b72c46c79597ad9ffbf35df64b1831a66264a35
GamingTrend <rafeole123@gmail.com>
2017-10-29 19:11:38 +0100

    Update GunGui.py

--- Multiplayer/Shop/Guns/GunGui.py
+++ Multiplayer/Shop/Guns/GunGui.py
@@ -12,9 +12,13 @@ image = Image.open('IMG_1592.JPG.png')
 image.show()
 import Image
 
-image = Image.open('IMG_1585.JPG.png')
+image = Image.open('m16-rifle-3500x2333-m16a1-m4a1-u-s-army-bullets-ammunition-camo-1721.jpg')
 image.show()
 import Image
 
-image = Image.open('IMG_1585.JPG.png')
+image = Image.open('pp_19_bizon_by_nazgono-d5j32vg.jpg')
+image.show()
+import Image
+
+image = Image.open('svd___dragunov_by_drake_uk-d4i8b63.jpg')
 image.show()
