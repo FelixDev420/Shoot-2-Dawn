6c20136e49c90a1c6a392ce3e19bad957904df0b
GamingTrend <rafeole123@gmail.com>
2017-10-29 17:16:08 +0100

    Create Register.py

---
+++ Multiplayer/PlayerID/Register.py
@@ -0,0 +1,14 @@
+c.itemconfig(worte, text='Welcome to Multiplayer Mode')
+c.itemconfig(worte, text='Signup')
+name = input ('Username ')
+password = input ('Password ')
+email = input ('Email or Telephone Number ')
+if a == name
+   and
+if a == password
+   and
+if a == email
+   c.itemconfig(worte, text='Signup')
+if a == Signup
+   print('Welcome', name)
+   
