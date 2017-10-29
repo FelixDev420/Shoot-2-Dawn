165c29f48575e52a7c1541aae06e8b0030b6a32a
GamingTrend <rafeole123@gmail.com>
2017-10-29 17:12:58 +0100

    Create PlayerPermissions.py

---
+++ Permission/PlayerPermissions.py
@@ -0,0 +1,32 @@
+def walk(event):
+    move()
+c.bind_all('<KeyPress-w>')
+def walk2(event):
+    move2()
+c.bind_all('<KeyPress-d>')
+def walk3(event):
+    move3()
+c.bind_all('<KeyPress-s>')
+def walk4(event):
+    move4()
+c.bind_all('<KeyPress-a>')
+def equip(event):
+    take()
+c.bind_all('<KeyPress-e>')
+def climb(event):
+    up()
+c.bind_all('<KeyPress-w>')
+if a == Ladder
+   climb = True
+else:
+   climb = False
+def attack(event):
+    fight()
+c.bind_all('<Button-3>')
+def reloading(event):
+    ammo()
+c.bind_all('<KeyPress-r>')
+def zoom(event):
+    scope()
+c.bind_all('<Button-1>')
+Player_fast = 5
