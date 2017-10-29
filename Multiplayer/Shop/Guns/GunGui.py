3b622e98dd383200af50b000d79807e02f423978
GamingTrend <rafeole123@gmail.com>
2017-10-29 18:43:45 +0100

    Update GunGui.py

--- Multiplayer/Shop/Guns/GunGui.py
+++ Multiplayer/Shop/Guns/GunGui.py
@@ -4,11 +4,11 @@ image = Image.open('IMG_1585.JPG.png')
 image.show()
 import Image
 
-image = Image.open('IMG_1585.JPG.png')
+image = Image.open('IMG_1588.JPG.png')
 image.show()
 import Image
 
-image = Image.open('IMG_1585.JPG.png')
+image = Image.open('IMG_1592.JPG.png')
 image.show()
 import Image
 
